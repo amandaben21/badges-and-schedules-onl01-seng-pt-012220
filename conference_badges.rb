0ca72008ff274ef0e4ffa93543e968db847a7132
Amanda Contreras <amandaben21@aol.com>
2020-02-02 22:31:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+
+def badge_maker (name)
+"Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+ attendees.collect do |name| 
+  badge_maker(name)
+end
+end
+
+def assign_rooms (attendees)
+  attendees.map.with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to rooms #{index+1}"
+  end
+end
+
+def printer (attendees)
+end
